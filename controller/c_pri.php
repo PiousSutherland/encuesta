9703b0302cf29b092716deb4029c2c45effec108
Pious Sutherland <sutherlandpious@gmail.com>
2024-03-31 11:07:00 +0200

    Previene ataques y ejecuta consultas

--- controller/c_pri.php
+++ controller/c_pri.php
@@ -1,8 +1,11 @@
 <?php
+// Iniciar la sesión para poder usar variables de sesión
+session_start();
 // Incluir el archivo de conexión
 include "model/conexion_bd.php";
 // Página 1: Datos Iniciales
 if (!empty($_POST["btnsig"])) {
+    // Sanitizar
     $nombre = htmlspecialchars($_POST["nombre"]);
     $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
     $sexo = htmlspecialchars($_POST["sexo"]);
@@ -11,9 +14,17 @@ if (!empty($_POST["btnsig"])) {
         echo "<script>alert('Por favor, rellena todos los campos.')</script>";
     else {
         // Insertar datos iniciales en la base de datos
-        $sql = "INSERT INTO moda (nombre, email, sexo) VALUES ('$nombre', '$email', '$sexo')";
+        $sql = "INSERT INTO moda (nombre, email, sexo) VALUES (?, ?, ?)";
 
-        if ($conexion->query($sql) === TRUE) {
+        // Preparar y vincular sentencias
+        $stmt = $conexion->prepare($sql);
+        if ($stmt === false) {
+            echo "Error al preparar la consulta: " . $conexion->error;
+            exit(); // or handle the error appropriately
+        }
+        $stmt->bind_param("sss", $nombre, $email, $sexo);
+
+        if ($stmt->execute()) {
             // Obtener el ID recién insertado
             $id_encuesta = $conexion->insert_id;
 
@@ -21,7 +32,10 @@ if (!empty($_POST["btnsig"])) {
             header("Location: views/seg.php?id_encuesta=$id_encuesta");
             exit();
         } else {
-            echo "Error al insertar datos: " . $conexion->error;
+            echo "Error al insertar datos: " . $stmt->error;
         }
+
+        // Cerrar conexion
+        $stmt->close();
     }
 }
